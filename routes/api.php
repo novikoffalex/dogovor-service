03cfe43e6b0f859c57e2aa4815660e3e6a66ca5f
Alexei Novikov <a.novikov@wanttopay.app>
2025-09-12 12:39:40 +0500

    Add test-pdf route

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ManychatContractController;
 
 Route::post('/manychat/contract', [ManychatContractController::class, 'generate']);
+Route::get('/test-pdf', [ManychatContractController::class, 'testPdf']);
 Route::get('/ping', fn() => 'pong');
