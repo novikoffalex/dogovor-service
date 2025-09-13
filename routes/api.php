44083b3acf4ddfb5728f4849b021c110f8bc5a66
Alexei Novikov <a.novikov@wanttopay.app>
2025-09-13 12:11:50 +0500

    Return JSON with contract_url for ManyChat integration
    - Save DOCX to public storage instead of temp
    - Return JSON response with contract_url, filename, contract_number
    - ManyChat can now use contract_url for file download

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ManychatContractController;
 
 Route::post('/manychat/contract', [ManychatContractController::class, 'generate']);
 Route::get('/manychat/contract/pdf/{filename}', [ManychatContractController::class, 'getPdf']);
-Route::post('/zamzar/webhook', [ManychatContractController::class, 'zamzarWebhook']);
 Route::get('/test-pdf', [ManychatContractController::class, 'testPdf']);
-Route::get('/test-pandoc', [ManychatContractController::class, 'testPandoc']);
 Route::get('/ping', fn() => 'pong');
